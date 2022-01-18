5b326d61e497fbea5000ffcd8e51302ee1faa1e4
Mo Asche <masche@lcs.on.ca>
2022-01-18 09:24:52 -0500

    Initial Commit

---
+++ Shared/TreckrMultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreckrMultiplatformApp.swift
+//  Shared
+//
+//  Created by Moritz Conrad Asche on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TreckrMultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
